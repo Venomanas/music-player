36e6383734b5802b26c95051a8121d62359fc04a
anas <anassayyed000@gmail.com>
2025-11-22 13:06:31 +0530

    01 app setup done

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images : {
+    remotePatterns: [
+      {
+        protocol:'https',
+        hostname: 'picsum.photos',
+      },{
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      //add more if you want { protocol : ..,..,.. }
+      {
+        protocol: 'https',
+        hostname:'files.freemusicarchive.org',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
